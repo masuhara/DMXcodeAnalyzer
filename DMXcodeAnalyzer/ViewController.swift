3be0644fbb392032ae575bd04b6ab2e7a0b6dabd
masuhara <info@masuhara.net>
2016-11-19 21:06:14 +0900

    Initial Commit

---
+++ DMXcodeAnalyzer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DMXcodeAnalyzer
+//
+//  Created by Masuhara on 2016/11/19.
+//  Copyright © 2016年 net.masuhara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
